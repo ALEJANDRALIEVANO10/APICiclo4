fdddf68c8ac19ac01ecd381a73aedc6d4957cbe7
Johnkarlv <johnkarlv@hotmail.com>
2022-11-03 20:44:16 -0400

    second commit

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+export namespace configuracion {
+  export const claveJWT = "MisionTIC";
+  export const baseURL = "http://127.0.0.1:5000/";
+  export const cadenaBD = "mongodb+srv://admin:admin750926@misionticcluster.qinbdxt.mongodb.net/test";
+  export const tipoComunicacion = 'email';
+}
